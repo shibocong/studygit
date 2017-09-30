4ed4d6904e2bf2c38b1c22c8a07bc94be6e985ff
shibocong <763564620@qq.com>
2017-09-30 15:28:48 +0800

    add one line #####

--- lanmp.sh
+++ lanmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## written by aming.
 ## 2015-06-24.
-
+###############################################################
 #######Begin########
 echo "It will install lamp or lnmp."
 sleep 1
